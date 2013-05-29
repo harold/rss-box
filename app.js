ee372eb52637a23df7a0bf21534a10b924ce3771
Harold <hhausman@gmail.com>
2013-05-28 22:56:21 -0600

    improvements
    - switching to feedparser, request, and cheerio
    - removes some boiler-plate
    - can handle smbc feed now by removing all `<script>` tags

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var _ = require('underscore');
 var path = require('path');
 var fs = require('fs');
-var http = require('http');
-var parseXMLString = require('xml2js').parseString;
+var request = require('request');
+var FeedParser = require('feedparser');
 var rimraf = require('rimraf');
+var cheerio = require('cheerio');
 
 var express = require('express');
 var app = express();
@@ -15,11 +16,13 @@ var hashString = function( str ) {
 	return crypto.createHash('md5').update(str).digest("hex");
 }
 
-if( !fs.existsSync('./data') )
-	fs.mkdirSync('./data', 0755 );
+var makeDir = function( path ) {
+	if( !fs.existsSync(path) )
+		fs.mkdirSync(path, 0755 );
+}
 
-if( !fs.existsSync('./data/feeds') )
-	fs.mkdirSync('./data/feeds', 0755 );
+makeDir( './data' );
+makeDir( './data/feeds' );
 
 var dir = function( path ) {
 	return _.filter( fs.readdirSync(path), function(path) {
@@ -27,87 +30,44 @@ var dir = function( path ) {
 	});
 }
 
-var getFeed = function( url, callback ) {
-	http.get(url, function( res ) {
-		var resBody = "";
-		res.on('data', function(data) {
-			resBody += data;
-		});
-
-		res.on('end', function() {
-			parseXMLString(resBody.toString(), function (err, result) {
-				if( err )
-					console.log("getFeed parseXMLString error: "+err);
-				else
-					callback(result);
-			});
-		});
-	}).on('error', function(e){console.log("getFeed error: "+e.message);});
-}
-
 var isString = function( s ) {
 	return ((typeof s).toLowerCase() == 'string' || s instanceof String);
 }
 
-var getContent = function( item ) {
-	if( item["content:encoded"] && isString(item["content:encoded"][0]) )
-		return item["content:encoded"][0];
-	if( item.description && isString(item.description[0]) )
-		return item.description[0];
-
-	var content = "<pre>Where's this item's content?!?:\n\n"+JSON.stringify(item)+"</pre>";
-	return content;
-}
-
-var getId = function( item ) {
-	var id = item.guid[0];
-	if( isString(id) )
-		return id;
-	if( isString(id._) )
-		return id._;
-
-	console.log( "\n***\n" );
-	console.log( "This id ain't no string: ", id );
-	console.log( "\n***\n" );
-}
-
 var processFeed = function( url ) {
-	getFeed( url, function( feed ) {
-		var feedId = hashString(url);
-		var folder = './data/feeds/'+feedId;
-		if( !fs.existsSync(folder) )
-			fs.mkdirSync(folder, 0755 );
+	console.log( "Updating: " + url );
+	var feedId = hashString(url);
+	var folder = './data/feeds/'+feedId;
+	makeDir( folder );
+	request(url).pipe(new FeedParser())
+	.on('error', function(error) { console.log("feedparser error:", error); })
+	.on('meta', function (meta) {
 		var outFeed = {
 			url: url,
-			title: feed.rss.channel[0].title[0],
-			link: feed.rss.channel[0].link[0],
+			title: meta.title,
+			link: meta.link,
 			lastFetched: (new Date).getTime()
 		};
 		fs.writeFileSync(folder+"/index.json", JSON.stringify(outFeed) );
-
-		var items = feed.rss.channel[0].item;
-		items = _.map( items, function(item) {
-			return {
-				title: item.title[0],
-				link: item.link[0],
-				content: getContent(item),
-				date: Date.parse(item.pubDate[0]),
-				id: hashString(getId(item)),
-				feedId: feedId,
-				feedLink: outFeed.link,
-				feedTitle: outFeed.title
-			}
-		});
-
+	})
+	.on('article', function (article) {
 		var itemFolder = folder+'/items';
-		if( !fs.existsSync(itemFolder) )
-			fs.mkdirSync(itemFolder, 0755 );
-
-		_.each( items, function(item) {
-			var itemPath = itemFolder+"/"+item.id+".json";
-			if( !fs.existsSync(itemPath) )
-				fs.writeFileSync(itemPath, JSON.stringify(item));
-		});
+		makeDir( itemFolder );
+		var $ = cheerio.load(article.description || article.summary);
+		$("script").remove();
+		var item = {
+			title: article.title,
+			link: article.link,
+			content: $.html(),
+			date: Date.parse(article.pubDate),
+			id: hashString(article.guid),
+			feedId: feedId,
+			feedLink: article.meta.link,
+			feedTitle: article.meta.title
+		}
+		var itemPath = itemFolder+"/"+item.id+".json";
+		if( !fs.existsSync(itemPath) )
+			fs.writeFileSync(itemPath, JSON.stringify(item));
 	});
 }
 
@@ -155,8 +115,7 @@ app.get('/readinglist', function(req, res) {
 
 app.post('/markasread', function(req, res) {
 	var feedReadFolder = "./data/feeds/"+req.body.feedId+"/read";
-	if( !fs.existsSync(feedReadFolder) )
-		fs.mkdirSync(feedReadFolder, 0755 );
+	makeDir( feedReadFolder );
 	if( !fs.existsSync(feedReadFolder+"/"+req.body.id+".json") ) {
 		var path = feedReadFolder+"/"+req.body.id+".json";
 		var contents = JSON.stringify({readTime: (new Date).getTime()});
@@ -170,7 +129,7 @@ app.listen(port);
 console.log('Listening on port '+port);
 
 var msInAMinute = 1000*60;
-var fiveMinnutes = 5 * msInAMinute;
+var fiveMinutes = 5 * msInAMinute;
 var anHour = 60 * msInAMinute;
 
 var updateFeeds = function() {
@@ -180,10 +139,8 @@ var updateFeeds = function() {
 		var feedIndex = JSON.parse(fs.readFileSync(feedFolder+"/"+feed+"/index.json"));
 		var currentTime = (new Date).getTime();
 		var delta = currentTime - feedIndex.lastFetched;
-		if( delta > anHour ) { // update at most once an hour
-			console.log( "Updating: " + feedIndex.title );
+		if( delta > anHour ) // update at most once an hour
 			processFeed( feedIndex.url );
-		}
 	});
 };
 
@@ -191,4 +148,4 @@ updateFeeds();
 setInterval( function() {
 	console.log( "Waking up to update feeds..." );
 	updateFeeds();
-}, fiveMinnutes+(fiveMinnutes*Math.random())); // 5 minutes + 0-5 minutes.
+}, fiveMinutes+(fiveMinutes*Math.random())); // 5 minutes + 0-5 minutes.
